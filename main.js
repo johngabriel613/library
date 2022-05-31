28d76c10dedb540c31774c56ff9336ae2e30b2c7
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2022-05-31 14:13:25 +0800

    test

---
+++ main.js
@@ -0,0 +1,10 @@
+var header = document.querySelector('header')
+
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 0){
+        header.classList.add('sticky')
+    }else{
+        header.classList.remove('sticky')
+    }
+})
